60b1a2ca5c3105eeed9d8d11adac830b7ece4aef
bean-123 <amy.platt@hotmail.com>
2025-09-02 14:41:14 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { useState } from "react";
 function App() {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
+  const handleLeftClick = () => {};
+
+  const handleRightClick = () => {};
 
   return (
     <>
+      <p>{clicks.left}</p>
       <p>{left}</p>
-      <button onClick={() => setLeft(left + 1)}>Left</button>
+      <button onClick={handleLeftClick}></button>
 
       <p>{right}</p>
-      <button onClick={() => setRight(left + 1)}>Right</button>
+      <button onClick={handleRightClick}>Right</button>
+      <p>{clicks.right}</p>
     </>
   );
 }
